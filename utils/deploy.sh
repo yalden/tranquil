4b1f9c4e64fb9a46ad2b4db436a7dfb8071ebf80
yoooonn <ycourlee@qq.com>
2022-07-29 23:56:11 +0800

    = Update: flatten pom

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -6,7 +6,30 @@ GREEN=$(tput setaf 2)
 BLUE=$(tput setaf 4)
 RESET=$(tput sgr0)
 
+greenText() {
+    echo "${GREEN}$1${RESET}"
+}
+
+redText() {
+    echo "${RED}$1${RESET}"
+}
+
 #set -eux
 set -eux
 
-./mvnw clean deploy -Prelease
+read -r -p "Will execute './mvnw clean deploy -Prelease' \
+$(redText 'Are You Sure?') [Y/n] " input
+
+case $input in
+[yY][eE][sS] | [yY])
+    greenText "Yes"
+    ./mvnw clean deploy -Prelease
+    greenText '\n\nDone'
+    exit 0
+    ;;
+
+*)
+    greenText "Stopped..."
+    exit 0
+    ;;
+esac
